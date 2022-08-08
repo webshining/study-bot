73aef92b4da54e5a391fd34cab216691dd9b9801
webshining <artemilienkono@gmail.com>
2022-08-09 02:47:03 +0300

    Update services

--- database/services/days.py
+++ database/services/days.py
@@ -1,8 +1,5 @@
 from ..models import Day
 
 
-def get_week(week: int):
-    if week % 2 == 0:
-        return list(Day.select())[7:]
-    else:
-        return list(Day.select())[:7]
+def get_week_subjects(week: int):
+    return [d.subjects for d in (list(Day.select())[:7] if week % 2 != 0 else list(Day.select())[7:])]
