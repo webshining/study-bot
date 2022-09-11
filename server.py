0ae01f0ecf1f0ffe167f2d1265ab4fa191622849
WebShining <artemilienkono@gmail.com>
2022-09-12 00:44:25 +0300

    Update app

--- server.py
+++ server.py
@@ -9,11 +9,12 @@ app.include_router(subjectsRouter)
 origins = [
     "https://react-dashboard-black.vercel.app"
 ]
-app.add_middleware(
-    CORSMiddleware,
-    allow_methods=['*'],
-    allow_headers=['*'],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_credentials=True,
+#     allow_methods=['*'],
+#     allow_headers=['*'],
+# )
 
 
 @app.get('/')
