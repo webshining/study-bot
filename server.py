2ba51fe0b27cd178ad78ae2157c9a9d533d9566c
WebShining <artemilienkono@gmail.com>
2022-09-12 00:21:26 +0300

    Update app

--- server.py
+++ server.py
@@ -12,8 +12,12 @@ origins = [
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['https://react-dashboard-black.vercel.app/'],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
 )
+
+
+if __name__ == '__main__':
+    uvicorn.run('server:app', reload=True, host='0.0.0.0', port=4000)
