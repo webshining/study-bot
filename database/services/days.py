1af41cec366cb09b6d968a695175a22deaf99056
WebShining <webshining@protonmail.com>
2022-10-25 22:05:41 +0300

    fix get days with subjects query

--- database/services/days.py
+++ database/services/days.py
@@ -2,15 +2,20 @@ from datetime import date
 from bson import ObjectId
 
 from ..models import Day, days_collection
-from .subjects import get_subject
 
 
 def get_days(week: int = None):
-    days = days_collection.find().sort("day_id")
-    days = [{**d, 'subjects': [{**get_subject(s['_id']).dict(), **s} for s in d['subjects']]} for d in days]
-    days = [Day(**d) for d in days]
+    days = days_collection.aggregate([
+        {"$unwind": {"path": "$subjects", "preserveNullAndEmptyArrays": True}},
+        {"$lookup": {"from": "subjects", "localField": "subjects._id", "foreignField": "_id", "as": "subjects.subject"}},
+        {"$unwind": {"path": "$subjects.subject", "preserveNullAndEmptyArrays": True}},
+        {"$group": {"_id": "$_id", "subjects": {"$push": "$subjects"}, "day_id": {"$first": "$day_id"}}},
+        {"$sort": {"day_id": 1}},
+        {"$project": {"subjects": {"$cond": {"if": {"$eq": ["$subjects", [{}]]}, "then": [], "else": "$subjects"}}}}
+    ])
     if week:
-        days = days[7:] if week%2!=0 else days[:7]
+        days = list(days)[7:] if week%2!=0 else list(days)[:7]
+    days = [Day(**d) for d in days]
     return days
 
 def create_days(ids):
