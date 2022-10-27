ddd9275d1aefdc18ba66a4dd8864fc2ebc2b63c8
WebShining <webshining@protonmail.com>
2022-10-27 20:20:59 +0300

    delete unused row

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from utils import logger
 async def on_startup(dispatcher):
     from app.commands import set_default_commands
     await set_default_commands()
-    from database import edit_day
     logger.info('Bot started!')
 
 
