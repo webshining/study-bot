6833876f3999351d509dabdb52f70f8ee82ffdaf
WebShining <webshinings@gmail.com>
2022-10-27 20:19:32 +0300

    add group to subjects

--- database/services/days.py
+++ database/services/days.py
@@ -23,18 +23,6 @@ def create_days(ids):
         days_collection.insert_one({'_id': ObjectId(id), 'subjects': []})
     return True
 
-def init_days():
-    if len(get_days()) != 14:
-        for i in range(14):
-            days_collection.insert_one({'subjects': []})
-    
-
-def edit_day(id: str, subjects):
-    day = days_collection.find_one_and_update({'_id': ObjectId(id)}, {'$set': {
-        'subjects': subjects
-    }})
-    return day 
-
 
 def get_day_by_date(_date: date) -> Day:    
     return get_days(_date.isocalendar().week)[_date.weekday()]
