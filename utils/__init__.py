76c970314ffa541e746962db4be1cbd0ffa6024d
WebShining <webshining@protonmail.com>
2022-10-28 08:43:50 +0300

    add get current time function

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,2 @@
 from .logging import logger
+from .current import get_current_time
---
+++ utils/current.py
@@ -0,0 +1,6 @@
+from pytz import timezone
+from datetime import datetime
+
+
+def get_current_time(time_zone: str = 'Europe/Kiev'):
+    return datetime.now(timezone(time_zone))
