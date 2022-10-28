d685f12b8ac105dc66de409905f7c84acbe76f5b
WebShining <webshining@protonmail.com>
2022-10-28 15:00:15 +0300

    update day

--- database/models/day.py
+++ database/models/day.py
@@ -4,13 +4,15 @@ from loader import db
 from .subject import Subject
 from .mongo import PydanticObjectId
 
+
 class DaySubject(BaseModel):
     id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias="_id")
     time_start: str
     time_end: str
     group: str = None
     subject: Subject
-    
+
+
 class Day(BaseModel):
     id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias="_id")
     subjects: list[DaySubject]
--- database/services/days.py
+++ database/services/days.py
@@ -7,16 +7,18 @@ from ..models import Day, days_collection
 def get_days(week: int = None):
     days = days_collection.aggregate([
         {"$unwind": {"path": "$subjects", "preserveNullAndEmptyArrays": True}},
-        {"$lookup": {"from": "subjects", "localField": "subjects._id", "foreignField": "_id", "as": "subjects.subject"}},
+        {"$lookup": {"from": "subjects", "localField": "subjects._id", "foreignField": "_id",
+                     "as": "subjects.subject"}},
         {"$unwind": {"path": "$subjects.subject", "preserveNullAndEmptyArrays": True}},
         {"$group": {"_id": "$_id", "subjects": {"$push": "$subjects"}, "day_id": {"$first": "$day_id"}}},
         {"$sort": {"day_id": 1}},
         {"$project": {"subjects": {"$cond": {"if": {"$eq": ["$subjects", [{}]]}, "then": [], "else": "$subjects"}}}}
     ])
     if week:
-        days = list(days)[7:] if week%2!=0 else list(days)[:7]
+        days = list(days)[7:] if week % 2 != 0 else list(days)[:7]
     days = [Day(**d) for d in days]
     return days
+
 
 def create_days(ids):
     for id in ids:
@@ -24,5 +26,11 @@ def create_days(ids):
     return True
 
 
-def get_day_by_date(_date: date) -> Day:    
-    return get_days(_date.isocalendar().week)[_date.weekday()]
+def edit_day(id: str, **kwargs):
+    day = days_collection.find_one_and_update({"_id": ObjectId(id)}, {"$set": kwargs})
+    return Day(**day)
+
+
+def get_day_by_date(_date: date) -> Day:
+    day = get_days(_date.isocalendar().week)[_date.weekday()]
+    return day
