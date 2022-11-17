24d59442060249d3bbd90426fa48eebb554ffdd8
WebShining <webshining@protonmail.com>
2022-11-18 01:43:59 +0200

    add admin filter

--- app/filters/admin.py
+++ app/filters/admin.py
@@ -1,41 +1,10 @@
-<<<<<<< HEAD
-from aiogram.types import Update
+from aiogram.types import Message
 from aiogram.filters import BaseFilter
 
 from data.config import ADMINS
 
 
 class AdminFilter(BaseFilter):
-    def __init__(self):
-        self.admins = ADMINS
-
-    def __call__(self, update: Update) -> bool:
-        global from_user
-        if update.message:
-            from_user = update.message.from_user
-        if update.callback_query:
-            from_user = update.callback_query.from_user
-        if update.inline_query:
-            from_user = update.inline_query.from_user
-        return from_user.id in self.admins
-=======
-from aiogram.types import Update
-from aiogram.filters import BaseFilter
-
-from data.config import ADMINS
-
-
-class AdminFilter(BaseFilter):
-    def __init__(self):
-        self.admins = ADMINS
-
-    def __call__(self, update: Update) -> bool:
-        global from_user
-        if update.message:
-            from_user = update.message.from_user
-        if update.callback_query:
-            from_user = update.callback_query.from_user
-        if update.inline_query:
-            from_user = update.inline_query.from_user
-        return from_user.id in self.admins
->>>>>>> 8ac7a890d7da264576b462a0803f970fa33cd353
+    async def __call__(self, message: Message) -> bool:
+        from_user = message.from_user
+        return from_user.id in ADMINS
