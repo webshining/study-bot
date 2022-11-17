42be01344e1ef587462ada897b2fbf06450e43cb
webshining <webshinings@gmail.com>
2022-11-18 01:45:35 +0200

    add lists keyboard

--- app/keyboards/__init__.py
+++ app/keyboards/__init__.py
@@ -1,4 +1,4 @@
 from .subjects import get_subjects_makrup
 from .week import get_week_makrup
 from .update import get_update_makrup
-from .files import get_files_makrup
+from .lists import get_lists_makrup
