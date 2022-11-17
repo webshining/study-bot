daabd10f6e45d74c3bbfa781f7540c664759d561
WebShining <webshining@protonmail.com>
2022-11-18 01:46:07 +0200

    add lists model

---
+++ database/models/list.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field
+
+from loader import db
+from .mongo import PydanticObjectId
+
+
+class ListElement(BaseModel):
+    name: str
+    value: str
+    user_id: int
+
+class List(BaseModel):
+    id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias="_id")
+    name: str
+    elements: list[ListElement] = []
+
+
+lists_collection = db["lists"]
