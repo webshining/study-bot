f33cd710e714b0fd33276a2d34c73a5c08e586f0
WebShining <webshining@protonmail.com>
2022-11-19 01:30:44 +0200

    update get update markup name

--- app/keyboards/__init__.py
+++ app/keyboards/__init__.py
@@ -1,4 +1,4 @@
 from .subjects import get_subjects_makrup
 from .week import get_week_makrup
-from .update import get_update_makrup
+from .update import get_update_markup
 from .lists import get_lists_makrup
