66506ff68177df74764672ed12c14bf82a8b742a
webshining <webshinings@gmail.com>
2022-11-19 01:29:52 +0200

    fix update markup

--- app/keyboards/update.py
+++ app/keyboards/update.py
@@ -2,10 +2,10 @@ from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-def get_update_makrup(data: str):
+def get_update_markup(data: str, value: str = None):
     builder = InlineKeyboardBuilder()
     buttons = [
-        InlineKeyboardButton(text='🔄Update', callback_data=f'{data}_update'),
+        InlineKeyboardButton(text='🔄Update', callback_data=f'{data}{f"_{value}" if value else ""}'),
     ]
     builder.add(*buttons)
     builder.adjust(3)
