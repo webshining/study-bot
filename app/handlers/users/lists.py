e4ff18ab82df5b3a6d29ffe7fe46e597b08065f9
WebShining <webshining@protonmail.com>
2022-11-18 03:00:47 +0200

    add list_set handler

--- app/handlers/users/lists.py
+++ app/handlers/users/lists.py
@@ -1,11 +1,14 @@
+import re
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
 
 from loader import dp
 from app.keyboards import get_lists_makrup
-from database import get_lists, get_list, List as ListModel
+from app.states import AddToList
+from database import get_lists, get_list, push_list_element, List as ListModel
 
-
+# Get lists
 @dp.message(Command('lists'))
 async def _lists(message: Message):
     text, markup = _get_lists_data()
@@ -18,8 +21,37 @@ async def _list(call: CallbackQuery):
     _list = get_list(call.data[10:])
     text = _get_list_text(_list) if _list else "List not found"
     await call.message.edit_text(text=text, reply_markup=None)
-    
-    
+
+# Add element to list
+@dp.message(Command("list_set"))
+async def _lists_set(message: Message):
+    if message.chat.type != 'private':
+        return message.answer("Во избежание всего плохого просим перейти вас в лс")
+    lists = get_lists()
+    await message.answer("Select list:" if lists else "Lists is emty", reply_markup=get_lists_makrup('lists_set', lists) if lists else None)
+
+
+@dp.callback_query(lambda call: call.data.startswith('lists_set'))
+async def _list_set(call: CallbackQuery, state: FSMContext):
+    _list = get_list(call.data[10:])
+    await state.update_data(id=_list.id)
+    await call.message.edit_text("Enter your name and value <b>[Kashey Immortal - world literature]</b>:" if _list else "List not found", reply_markup=None)
+    if _list:
+        await state.set_state(AddToList.text)
+
+
+@dp.message(AddToList.text)
+async def _list_set_value(message: Message, state: FSMContext):
+    id = (await state.get_data()).get('id')
+    match = re.match("(.*) - (.*)", message.text)
+    if match:
+        push_list_element(id, match[1], match[2], message.from_user.id)
+        await message.answer("Lists updated")
+        await state.clear()
+    else:
+        await message.answer("Incorrect input data format, please enter correctly")
+
+
 def _get_list_text(list: ListModel):
     text = f"<b>{list.name}</b>"
     for element in list.elements:
