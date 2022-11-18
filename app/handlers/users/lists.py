f50179675ccf58ce26d5124e0155951fac487fe5
WebShining <webshining@protonmail.com>
2022-11-18 16:55:28 +0200

    clear code

--- app/handlers/users/lists.py
+++ app/handlers/users/lists.py
@@ -3,80 +3,79 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 
-from loader import dp, bot
-from app.keyboards import get_lists_makrup, get_update_makrup, get_to_private_makrup
+from loader import dp
+from app.keyboards import get_lists_makrup, get_update_markup, get_to_private_makrup
 from app.states import AddToList
 from database import get_lists, get_list, push_list_element, List as ListModel
 
 # Get lists
 @dp.message(Command('lists'))
 async def _lists(message: Message):
-    text, markup = _get_lists_data()
+    text, markup = _get_lists_data("Select a list to view:", 'lists_get')
     await message.answer(text, reply_markup=markup)
     
     
-@dp.callback_query(lambda call: call.data.startswith('lists_get') or call.data.startswith('lists_upd'))
-async def _list(call: CallbackQuery):
+@dp.callback_query(lambda call: call.data.startswith('lists_get'))
+async def _lists_list(call: CallbackQuery):
     await call.answer()
     _list = get_list(call.data[10:])
-    text = _get_list_text(_list) if _list else "List not found"
+    text, markup = _get_list_data(_list)
     try:
-        markup = get_update_makrup('lists_upd', call.data[10:])
-        if call.data[6:].startswith('upd'):
-            if call.inline_message_id:
-                return await bot.edit_message_text(text=text, inline_message_id=call.inline_message_id, reply_markup=markup)
-            await call.message.edit_text(text=text, reply_markup=markup)
-        else:
-            if call.inline_message_id:
-                return await bot.edit_message_text(text=text, inline_message_id=call.inline_message_id, reply_markup=markup)
-            await call.message.edit_text(text=text, reply_markup=markup)
+        await call.message.edit_text(text, reply_markup=markup)
     except:
         pass
 
 # Add element to list
 @dp.message(Command("lists_set"))
 async def _lists_set(message: Message):
-    if message.chat.type != 'private':
-        return message.answer("In order to avoid all the bad, please go to private chat", reply_markup=get_to_private_makrup())
-    lists = get_lists()
-    await message.answer("Select list:" if lists else "Lists is emty", reply_markup=get_lists_makrup('lists_set', lists) if lists else None)
+    text, markup = _get_lists_data("Select a list to set:", 'lists_set')
+    await message.answer(text, reply_markup=markup)
 
 
 @dp.callback_query(lambda call: call.data.startswith('lists_set'))
-async def _list_set(call: CallbackQuery, state: FSMContext):
+async def _lists_set_callback(call: CallbackQuery, state: FSMContext):
     await call.answer()
     _list = get_list(call.data[10:])
-    await state.update_data(id=_list.id)
-    await call.message.answer(f"<b>{call.from_user.full_name}</b> enter your name and value <b>[Kashey Immortal - world literature]</b>:" if _list else "List not found of deleted", reply_markup=None)
     if _list:
+        await state.clear()
+        await call.message.answer(f"<b>{call.from_user.full_name}</b> enter the format string <b>Name - Value</b>:")
+        await state.update_data(id=_list.id)
         await state.set_state(AddToList.text)
+    else:
+        await call.message.edit_text("List not found or deleted", reply_markup=None)
 
 
 @dp.message(AddToList.text)
-async def _list_set_value(message: Message, state: FSMContext):
-    id = (await state.get_data()).get('id')
+async def _list_set_text(message: Message, state: FSMContext):
     match = re.match("(.*) - (.*)", message.text)
     if match:
-        push_list_element(id, match[1], match[2], message.from_user.id)
-        await message.answer("Lists updated")
+        id = (await state.get_data()).get('id')
+        list = get_list(id)
+        if list:
+            push_list_element(id, match[1], match[2], message.from_user.id)
+            await message.answer("Lists updated")
+        else:
+            await message.answer("List not found or deleted")
         await state.clear()
     else:
         await message.answer("Incorrect input data format, please enter correctly")
 
 
-def _get_list_text(list: ListModel):
-    text = f"<b>{list.name}</b>"
-    for element in list.elements:
-        text += f'\n{element.name} - {element.value}'
-    if not list.elements:
-        text += f'\nList is empty🫡'
-    
-    return text
-        
-    
+def _get_list_data(list: ListModel | None):
+    text = "List not found"
+    markup = None
+    if list:
+        text = f"List <b>{list.name}</b> is empty"
+        markup = get_update_markup('lists_get', list.id)
+        if list.elements:
+            text = f'<b>{list.name}</b>'
+            for element in list.elements:
+                text += f'\n{element.name} - {element.value}'
+    return text, markup
 
-def _get_lists_data():
+
+def _get_lists_data(text: str, data: str):
     lists = get_lists()
-    markup = get_lists_makrup('lists_get', lists) if lists else None
+    markup = get_lists_makrup(data, lists) if lists else None
     
-    return "Select list:" if lists else "Lists is empty🫡", markup
+    return text if lists else "Lists is empty🫡", markup
