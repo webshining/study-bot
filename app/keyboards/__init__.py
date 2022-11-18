c81d40589be132f2ecce6a78666fe4198b6355e8
webshining <webshinings@gmail.com>
2022-11-18 16:55:07 +0200

    edit get update markup name

--- app/keyboards/__init__.py
+++ app/keyboards/__init__.py
@@ -1,5 +1,5 @@
 from .subjects import get_subjects_makrup
 from .week import get_week_makrup
-from .update import get_update_makrup
+from .update import get_update_markup
 from .lists import get_lists_makrup
 from .to_private import get_to_private_makrup
