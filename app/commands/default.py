6ea4611d0776bdd88988bb2af86a351e589a7d8b
WebShining <webshining@protonmail.com>
2022-11-18 03:00:01 +0200

    edit default commands

--- app/commands/default.py
+++ app/commands/default.py
@@ -6,10 +6,12 @@ from loader import bot
 def get_default_commands():
     commands = [
         BotCommand(command='/start', description='start message'),
-        BotCommand(command='/help', description='how is works'),
         BotCommand(command='/schedule', description='schedule'),
         BotCommand(command='/subjects', description='subjects info'),
-        BotCommand(command='/current', description='current subject')
+        BotCommand(command='/current', description='current subject'),
+        BotCommand(command='/lists', description='get lists'),
+        BotCommand(command='/lists_set', description='add an entry to the list'),
+        BotCommand(command='/help', description='how is works'),
     ]
     return commands
 
