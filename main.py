604f9e502af652dc847d2671419ebad9854a085e
WebShining <webshining@protonmail.com>
2022-11-24 02:41:02 +0200

    remove admin commands on shutdown

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from aiogram.types import BotCommandScopeDefault
 
 from loader import dp, bot
+from data.config import ADMINS
 
 
 async def on_startup():
-    # from app.commands import set_default_commands
-    # await set_default_commands()
     from database import init_days
     init_days()
+    from app.commands import set_default_commands
+    await set_default_commands()
     print('Bot started!')
 
 
 async def on_shutdown():
-    # await bot.delete_my_commands(scope=BotCommandScopeDefault())
+    from app.commands import remove_admins_command    
+    await bot.delete_my_commands(scope=BotCommandScopeDefault())
+    for admin in ADMINS:
+        await remove_admins_command(admin)
     print('Bot shutting down!')
 
     
 def main():
-    from app.middlewares import setup_middleware
     import app.handlers
+    from app.middlewares import setup_middleware
     dp.startup.register(on_startup)
     dp.shutdown.register(on_shutdown)
     setup_middleware(dp)
