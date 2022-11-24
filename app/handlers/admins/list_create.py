de24375abc944c147a8969a1a480e39cc008e4d0
webshining <webshinings@gmail.com>
2022-11-24 03:13:09 +0200

    add admin filter

--- app/handlers/admins/list_create.py
+++ app/handlers/admins/list_create.py
@@ -4,16 +4,17 @@ from aiogram.fsm.context import FSMContext
 
 from loader import dp, _
 from app.states import ListCreate
+from app.filters import AdminFilter
 from database import create_list
 
 
-@dp.message(Command("list_create"))
+@dp.message(AdminFilter(), Command("list_create"))
 async def _list_create(message: Message, state: FSMContext):
     await message.answer(_("Enter list name:"))
     await state.set_state(ListCreate.name)
 
 
-@dp.message(ListCreate.name)
+@dp.message(AdminFilter(), ListCreate.name)
 async def _list_create_name(message: Message, state: FSMContext):
     create_list(message.text)
     await message.answer(_("List created successfully"))
