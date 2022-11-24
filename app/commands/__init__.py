e64f6de46b6850abcd9a33ec4a48cb90ee91e97f
webshining <webshinings@gmail.com>
2022-11-24 02:42:38 +0200

    update imports

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -1,2 +1,2 @@
-from .default import set_default_commands
-from .admins import set_admins_commands, remove_admins_command
+from .default import set_default_commands, get_default_commands
+from .admins import set_admins_commands, remove_admins_command, get_admins_commands
