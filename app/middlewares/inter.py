94216bdb21dc115f66c0537b5224d7ab514aefef
WebShining <webshining@protonmail.com>
2022-11-24 02:41:32 +0200

    add i18n middleware

---
+++ app/middlewares/inter.py
@@ -0,0 +1,7 @@
+from aiogram.utils.i18n import I18n, SimpleI18nMiddleware
+
+from data.config import I18N_DOMAIN, I18N_PATH
+
+
+i18n = I18n(path=I18N_PATH, domain=I18N_DOMAIN)
+i18n_middleware = SimpleI18nMiddleware(i18n)
