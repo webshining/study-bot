b1d7aa1547becd73a35589d39cec10b84709610a
WebShining <webshining@protonmail.com>
2022-11-24 03:12:27 +0200

    fix admin filter

--- app/filters/admin.py
+++ app/filters/admin.py
@@ -6,10 +6,4 @@ from data.config import ADMINS
 
 class AdminFilter(BaseFilter):
     async def __call__(self, update: Update) -> bool:
-        if update.message:
-            from_user = update.message.from_user
-        elif update.callback_query:
-            from_user = update.callback_query.from_user
-        elif update.inline_query:
-            from_user = update.inline_query.from_user
-        return from_user.id in ADMINS
+        return update.from_user.id in ADMINS
