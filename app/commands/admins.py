bb8b4f03c6406c9e7e66b8a8c76015b36c1eac07
WebShining <webshining@protonmail.com>
2022-11-24 02:42:52 +0200

    translate commands

--- app/commands/admins.py
+++ app/commands/admins.py
@@ -1,10 +1,10 @@
 from aiogram.types import BotCommand, BotCommandScopeChat
 
-from loader import bot
+from loader import bot, i18n, _
 from .default import get_default_commands
 
 
-def get_admins_commands():
+def get_admins_commands(lang: str = 'en'):
     commands = get_default_commands()
     commands.extend([])
     return commands
@@ -12,8 +12,13 @@ def get_admins_commands():
 
 async def set_admins_commands(id: int):
     await bot.set_my_commands(get_admins_commands(), scope=BotCommandScopeChat(chat_id=id))
+    for lang in i18n.available_locales:
+        await bot.set_my_commands(get_admins_commands(lang), scope=BotCommandScopeChat(chat_id=id), language_code=lang)
+        
 
 
 async def remove_admins_command(id: int):
-    await bot.set_my_commands(get_default_commands(), scope=BotCommandScopeChat(chat_id=id))
+    await bot.delete_my_commands(scope=BotCommandScopeChat(chat_id=id))
+    for lang in i18n.available_locales:
+        await bot.delete_my_commands(scope=BotCommandScopeChat(chat_id=id), language_code=lang)
     
--- app/commands/default.py
+++ app/commands/default.py
@@ -1,20 +1,22 @@
 from aiogram.types import BotCommand, BotCommandScopeDefault
 
-from loader import bot
+from loader import bot, i18n, _
 
 
-def get_default_commands():
+def get_default_commands(lang: str = 'en'):
     commands = [
-        BotCommand(command='/start', description='start message'),
-        BotCommand(command='/schedule', description='schedule'),
-        BotCommand(command='/subjects', description='subjects info'),
-        BotCommand(command='/current', description='current subject'),
-        BotCommand(command='/cancel', description='reset state'),
-        BotCommand(command='/help', description='how is works'),
+        BotCommand(command='/start', description=_('start message', locale=lang)),
+        BotCommand(command='/schedule', description=_('schedule', locale=lang)),
+        BotCommand(command='/subjects', description=_('subjects info', locale=lang)),
+        BotCommand(command='/current', description=_('current subject', locale=lang)),
+        BotCommand(command='/cancel', description=_('reset state', locale=lang)),
+        BotCommand(command='/help', description=_('how is works', locale=lang)),
     ]
     return commands
 
 
 async def set_default_commands():
     await bot.set_my_commands(get_default_commands(), scope=BotCommandScopeDefault())
+    for lang in i18n.available_locales:
+        await bot.set_my_commands(get_default_commands(lang), scope=BotCommandScopeDefault(), language_code=lang)
     
